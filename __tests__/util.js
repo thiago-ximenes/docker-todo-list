412f040264e9868319d3e227e776c29c7b6021d4
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-02-17 15:49:23 -0300

    passa no requisito 2

--- __tests__/util.js
+++ __tests__/util.js
@@ -1,7 +1,6 @@
 const { resolve } = require('path');
 const { promisify } = require('util');
 const { log: consoleLog, warn: consoleWarn } = require('console');
-// const { readFile } = require('fs/promises');
 const { readFile } = require('fs').promises;
 
 const { evalId } = require('./constants');
